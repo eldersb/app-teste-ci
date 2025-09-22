528bbd1023b0474ff2907a5753bc613036f6a30e
elder.sb <elders3288@outlook.com>
2025-09-22 16:05:11 +0000

    Github Actions

--- app/Views/produtos/index.php
+++ app/Views/produtos/index.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-RPD6C25L89"></script>
+   <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+    gtag('config', 'G-RPD6C25L89');
+  </script>
 </head>
 <body class="bg-light">
     <div class="container py-5">
